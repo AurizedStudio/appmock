f9ac9d8067011ed2d8e1a04bab94703eb0c0b05f
T.Suzaki <info@aurized-studio.jp>
2015-06-17 00:47:57 +0900

    グロナビ作成

--- htdocs/js/style.js
+++ htdocs/js/style.js
@@ -3,11 +3,18 @@
  */
 
 $(function(){
-
-	drawer();
-
-function drawer(){
+	"use strict";
 	$('.drawer').drawer();
-}
 
+//	var icon = $(".iconfont");
+
+//	$('.drawer').on('drawer.opened',function(){
+//		console.log('opened');
+//		icon.removeClass('iconfont-humberger').addClass('iconfont-close');	
+//	});
+
+//	$('.drawer').on('drawer.closed',function(){
+//		console.log('closed');
+//		icon.removeClass('iconfont-close').addClass('iconfont-humberger');	
+//	});
 });
